e39f1a5b6058f28beb0882e359a5d70977fc7f31
rutooya <117901340+rutooya@users.noreply.github.com>
2022-12-15 16:35:36 +0700

    Add files via upload

--- UG13_D_71220923.py
+++ UG13_D_71220923.py
@@ -1,46 +1,59 @@
 import turtle
 
 pen = turtle.Turtle()
-turtle.Screen().bgcolor("purple")
+turtle.Screen().bgcolor("violet")
 pen.pensize(6)
 
 #huruf D
 pen.up()
-pen.goto(-10,250)
+pen.goto(-10,270)
 pen.down()
-pen.pencolor("white")
+pen.pencolor("gold")
 pen.right(90)
 pen.forward(135)
 
 pen.up()
-pen.goto(-10,250)
+pen.goto(-10,270)
 pen.down()
 pen.right(-90)
 pen.circle(-70,180)
 
-#angka
+#angka 9
 pen.up()
-pen.goto(-200,70)
+pen.goto(-200,90)
 pen.down()
 pen.circle(50)
 pen.up()
-pen.goto(-151,30)
+pen.goto(-151,50)
 pen.down()
 pen.left(90)
 pen.forward(120)
 pen.right(90)
 pen.forward(100)
 
+#angka 2
 pen.up()
-pen.goto(-40,70)
+pen.goto(-40,90)
 pen.down()
 pen.right(180)
-pen.circle(-80,180)
+pen.circle(-78,180)
 pen.left(180)
 pen.forward(140)
 
+#angka 3
 pen.up()
-pen.goto(200,70)
+pen.goto(200,110)
 pen.down()
-pen.circle(-20,180)
+pen.circle(-50,180)
+pen.left(180)
+pen.circle(-50,180)
+
+#huruf P
+pen.up()
+pen.goto(-10,-270)
+pen.down()
+pen.right(90)
+pen.forward(145)
+pen.right(90)
+pen.circle(-40,180)
 s = turtle.Screen().exitonclick()
